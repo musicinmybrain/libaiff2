150aa2fe622cf3d4e7589287e5526be24cd8ed95
toad32767 <toad32767>
2007-09-20 17:13:16 +0000

    Add an example config.h for Microsoft Visual C platforms. Thanks to Anthony\!

---
+++ libaiff/config.vc.h
@@ -0,0 +1,27 @@
+/* 	$Id$ */
+/*
+ * Sample <config.h> file for Microsoft platforms
+ */
+
+typedef unsigned __int64_t 	uint64_t;
+typedef __int64_t 		int64_t;
+typedef unsigned int		uint32_t;
+typedef signed int 		int32_t;
+typedef unsigned short 		uint16_t;
+typedef signed short 		int16_t;
+typedef unsigned char 		uint8_t;
+typedef signed char 		int8_t;
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "marcotrillo@gmail.com" 
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "LibAiff (MSVC)"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "LibAiff (MSVC) $Revision$"
+
+/* Define to the version of this package. */ 
+#define PACKAGE_VERSION "$Revision$"
+
+
